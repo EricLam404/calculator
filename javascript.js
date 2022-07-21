1381d5bdd17720a0130c2edd18740cf5ee31fdec
EricLam404 <lameric9888@gmail.com>
2022-07-21 19:59:12 -0400

    added clear and delete

--- javascript.js
+++ javascript.js
@@ -14,3 +14,34 @@ let operate = (num1, operand, num2) => {
         divide(num1, num2)
     }
 }
+
+let buttons  = document.querySelectorAll(".btn");
+buttons.forEach(button => button.addEventListener("click", clicked));
+
+function clicked(e){
+    if(this.id == "clear"){
+        clear();
+        return
+    }
+    if(this.id == "delete"){
+        deleted();
+        return
+    }
+    let screenLast = document.querySelector(".screen-last");
+    let screenCurrent = document.querySelector(".screen-current");
+    screenLast.textContent += this.id;
+    screenCurrent.textContent += this.id;
+}
+
+function clear(){
+    let screenLast = document.querySelector(".screen-last");
+    let screenCurrent = document.querySelector(".screen-current");
+
+    screenLast.textContent = "";
+    screenCurrent.textContent = "";
+}
+
+function deleted(){
+    let screenCurrent = document.querySelector(".screen-current");
+    screenCurrent.textContent = screenCurrent.textContent.slice(0, screenCurrent.textContent.length-1);
+}
