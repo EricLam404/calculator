048fcf7113463f5e8d32b3a8631e8b7b4009542f
EricLam404 <lameric9888@gmail.com>
2022-07-21 20:31:18 -0400

    added 2 number arithmetic

--- javascript.js
+++ javascript.js
@@ -5,13 +5,13 @@ let divide = (num1, num2) => num1 / num2;
 
 let operate = (num1, operand, num2) => {
     if(operand === "+"){
-        add(num1, num2)
+        return add(num1, num2)
     } else if(operand === "-"){
-        subtract(num1, num2)
-    } else if(operand === "*"){
-        multiply(num1, num2)
-    } else if(operand === "/"){
-        divide(num1, num2)
+        return subtract(num1, num2)
+    } else if(operand === "×"){
+        return multiply(num1, num2)
+    } else if(operand === "÷"){
+        return divide(num1, num2)
     }
 }
 
@@ -29,8 +29,32 @@ function clicked(e){
     }
     let screenLast = document.querySelector(".screen-last");
     let screenCurrent = document.querySelector(".screen-current");
-    screenLast.textContent += this.id;
-    screenCurrent.textContent += this.id;
+
+
+    if(this.classList.contains("operand")){
+        if(+screenLast.textContent == screenLast.textContent){
+            screenLast.textContent += screenCurrent.textContent + " " + this.id;
+            screenCurrent.textContent = "";
+        }
+        screenLast.textContent = screenLast.textContent.slice(0, screenLast.textContent.length-2);
+        screenLast.textContent += screenCurrent.textContent + " " + this.id;
+        screenCurrent.textContent = "";
+        return
+    }
+    if(this.classList.contains("equal")){
+        if(+screenCurrent.textContent == screenCurrent.textContent){
+            if(+screenLast.textContent != screenLast.textContent){
+                console.log(operate(split[0], split[1], screenCurrent.textContent))
+                screenLast.textContent = operate(split[0], split[1], screenCurrent.textContent);
+                screenCurrent.textContent = "";
+                return;
+            }
+        }
+    }
+    if(+screenCurrent.textContent == screenCurrent.textContent){
+        screenCurrent.textContent += this.id;
+        screenCurrent.textContent = +screenCurrent.textContent;
+    }
 }
 
 function clear(){
@@ -43,5 +67,9 @@ function clear(){
 
 function deleted(){
     let screenCurrent = document.querySelector(".screen-current");
+    if(screenCurrent.textContent.length-1 == 0){
+        screenCurrent.textContent = 0;
+        return
+    }
     screenCurrent.textContent = screenCurrent.textContent.slice(0, screenCurrent.textContent.length-1);
 }
